ace4fd8a5c07d797fb4c7b8e944cbc7047f2055c
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-05-09 11:16:28 -0400

    refactor main

--- examples/Python/rrworker.py
+++ examples/Python/rrworker.py
@@ -5,11 +5,17 @@
 #
 import zmq
 
-context = zmq.Context()
-socket = context.socket(zmq.REP)
-socket.connect("tcp://localhost:5560")
 
-while True:
-    message = socket.recv()
-    print("Received request: %s" % message)
-    socket.send(b"World")
+def main():
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.connect("tcp://localhost:5560")
+
+    while True:
+        message = socket.recv()
+        print("Received request: %s" % message)
+        socket.send(b"World")
+
+
+if __name__ == "__main__":
+    main()
