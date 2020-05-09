8fca5a051896cec58e955f0812422c6e98a5a7be
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-05-09 11:15:37 -0400

    refactor main

--- examples/Python/rrclient.py
+++ examples/Python/rrclient.py
@@ -5,13 +5,21 @@
 #
 import zmq
 
-#  Prepare our context and sockets
-context = zmq.Context()
-socket = context.socket(zmq.REQ)
-socket.connect("tcp://localhost:5559")
 
-#  Do 10 requests, waiting each time for a response
-for request in range(1,11):
-    socket.send(b"Hello")
-    message = socket.recv()
-    print("Received reply %s [%s]" % (request, message))
+def main():
+    """
+    Do 10 requests, waiting each time for a response
+    :return:
+    """
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://localhost:5559")
+
+    for request in range(1, 11):
+        socket.send(b"Hello")
+        message = socket.recv()
+        print("Received reply %s [%s]" % (request, message))
+
+
+if __name__ == "__main__":
+    main()
