471bb6670f7e6402a83b7840147bab06bde3cf6d
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-05-10 13:10:23 -0400

    overlay strategy pattern

--- examples/Python/taskwork.py
+++ examples/Python/taskwork.py
@@ -9,6 +9,49 @@ import time
 
 import zmq
 
+from types import MethodType
+
+
+class Strategy:
+    """The Strategy Pattern class"""
+
+    def __init__(self, function):
+        self.name = "Default Strategy"
+        self.execute = MethodType(function, self)
+
+
+def strategy_one(self, payload):
+    """
+    # Replacement method 1
+    :param self:
+    :return:
+    """
+    self.name = "Strategy One"
+    time.sleep(payload['workload'])
+    print("{} is used to execute method 1".format(self.name))
+
+
+def strategy_two(self, payload):
+    """
+    # Replacement method 2
+    :param self:
+    :return:
+    """
+    self.name = "Strategy Two"
+    time.sleep(payload['workload'])
+    print("{} is used to execute method 2".format(self.name))
+
+
+def strategy_three(self, payload):
+    """
+    # Replacement method 3
+    :param self:
+    :return:
+    """
+    self.name = "Strategy Three"
+    time.sleep(payload['workload'])
+    print("{} is used to execute method 2".format(self.name))
+
 
 def main():
     context = zmq.Context()
@@ -25,8 +68,17 @@ def main():
         message_dict = vent.recv_json()
         print(f"start message_dict={message_dict}")
         time.sleep(message_dict['workload'])
-        print(f"done message_dict={message_dict}")
-        sink.send_json(message_dict)
+        strategy_str = message_dict['strategy'] if 'strategy' in message_dict else "strategy_one"
+        curr_strategy = Strategy(eval(strategy_str))
+        try:
+            curr_strategy.execute(message_dict)
+            message_dict['result'] = 'done'
+            sink.send_json(message_dict)
+            print(f"done message_dict={message_dict}")
+        except:
+            message_dict['result'] = 'failed'
+            sink.send_json(message_dict)
+            print("failed")
 
 
 if __name__ == "__main__":
